3dfa968e2fa293b8366b6464113d7ae8e90206ea
hujunjie <hujunjie@1token.trade>
2024-12-17 14:15:34 +0800

    html-template

--- config/index.ts
+++ config/index.ts
@@ -17,7 +17,7 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
     },
     sourceRoot: 'src',
     outputRoot: 'dist',
-    plugins: [],
+    plugins: ['@tarojs/plugin-html'],
     defineConstants: {},
     copy: {
       patterns: [],
@@ -26,7 +26,7 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
     framework: 'vue3',
     compiler: 'webpack5',
     cache: {
-      enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
     mini: {
       postcss: {
