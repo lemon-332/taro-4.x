60e1b78f871aa1b32c335a200ebd066dcc85ba40
hujunjie <hujunjie@1token.trade>
2024-12-17 16:57:39 +0800

    feat:http template

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="create">
-    <button class="btn" @click="goUserDetailPage">关于跳转</button>
+    <button class="btn" @click="goUserDetailPage">{{ name }}</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Taro from '@tarojs/taro'
+import { getBookList } from '@/services/apis/user'
+import { ref } from 'vue'
+
+const name = ref('张三')
 const goUserDetailPage = () => {
-  Taro.navigateTo({ url: '/pagesSub/about/index' })
+  _getBookList()
+}
+const _getBookList = async () => {
+  const res = await getBookList()
+  name.value = res.data
 }
 </script>
 
 <style lang="scss">
 .create {
   padding: 0 15px;
-  .btn{
+  .btn {
     display: inline;
   }
 }
